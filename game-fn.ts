cb0cf847d13539d7b8d394c2fc47914247b22820
Kroekpol noonpang <kroekpol.cpk@gmail.com>
2025-11-24 15:32:09 +0700

    ARRAY ASSIMENT

--- game-fn.ts
+++ game-fn.ts
@@ -1,30 +1,42 @@
-const board = [
-	["A", "B", "C"],
-	["D", "E", "F"],
-	["G", "H", "I"],
+const boardfn = [
+  ["A", "B", "C"],
+  ["D", "E", "F"],
+  ["G", "H", "I"],
 ];
-
-const moves = [];
-
+// console.log(boardfn.length);//row length
+// console.log(boardfn[0].length);//col length
+const rowLength = boardfn.length;
+const colLength = boardfn[0].length;
+const movefn: string[] = [];
 // Start at A (0,0) -> [row][col]
-let row = 0;
-let col = 0;
-moves.push(board[row][col]); // A
-
+let rowfn = 0;
+let colfn = 0;
+movefn.push(boardfn[rowfn][colfn]); // A
 // Define movement functions
-function moveRight() {}
-
-function moveLeft() {}
-
-function moveUp() {}
-
-function moveDown() {}
-
+function moveRight() {
+  if (colfn < colLength) {
+    colfn++;
+    movefn.push(boardfn[rowfn][colfn]);
+  } else {
+    console.log("Invalid move");
+  }
+}
+function moveLeft() {
+  if (colfn > 0) colfn--;
+  movefn.push(boardfn[rowfn][colfn]);
+}
+function moveUp() {
+  if (rowfn > 0) rowfn--;
+  movefn.push(boardfn[rowfn][colfn]);
+}
+function moveDown() {
+  if (rowfn < rowLength) rowfn++;
+  movefn.push(boardfn[rowfn][colfn]);
+}
 // Call the movement functions
 moveRight(); // Move from A to B
 moveRight(); // Move from B to C
 moveDown(); // Move from C to F
 moveLeft(); // Move from F to E
-
-console.log("Path:", moves.join(" → "));
-console.log("Total Moves (Start from 'A'):", moves.length - 1);
+console.log("Path:", movefn.join(" → "));
+console.log("Total Moves (Start from 'A'):", movefn.length - 1);
--- game.ts
+++ game.ts
@@ -9,17 +9,23 @@ const moves = [];
 // Start at A (0,0) -> [row][col]
 let row = 0;
 let col = 0; 
-moves.push(board[row][col]); // A
+moves.push(board[row][col]); // A // r0 c0 [0][0]
 
-// Move Right to B
+// Move Right to B		// ro c1 [0][1]
 col++;
 moves.push(board[row][col]); // B
 
-// Move Right to C
+// Move Right to C 		//r0 c2 [0][2]
+col++;
+moves.push(board[row][col]); // C
 
-// Move Down to F
+// Move Down to F		//r1 c2	[1][2]
+row++;
+moves.push(board[row][col]); // F
 
-// Move left to E
+// Move left to E	 	//r1 c1 [1][1]
+col--;
+moves.push(board[row][col]); // F
 
 console.log("Path:", moves.join(" → "));
 // Output: Path: A → B → C → F → E
